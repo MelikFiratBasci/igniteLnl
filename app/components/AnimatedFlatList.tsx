32e3f447a5104f145932c8ce7b4f9e5c9d598047
Melik Fırat Başçi <70327175+MelikFiratBasci@users.noreply.github.com>
2023-09-25 14:36:25 +0300

    animatedFlatList component is implemented

---
+++ app/components/AnimatedFlatList.tsx
@@ -0,0 +1,143 @@
+import React from "react"
+import {
+  View,
+  TouchableOpacity,
+  ViewStyle,
+  RefreshControl,
+  ActivityIndicator,
+  Image,
+} from "react-native"
+import { Text } from "./Text"
+import Animated, {
+  Easing,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated"
+import { colors, spacing } from "../theme"
+import { useDispatch } from "react-redux"
+import { useNavigation } from "@react-navigation/native"
+import { Card } from "./Card"
+
+interface Item {
+  id: number
+  title: string
+  image?: string
+  price?: number
+  qrcode?: string
+}
+
+interface AnimatedFlatListProps {
+  data: Item[]
+  loading: boolean
+  isRefreshing: boolean
+  onRefresh: () => void
+  onSearch: (searchTerm: string) => void
+  onItemPress: (item: Item) => void
+  translateY: Animated.SharedValue<number>
+  scrollHandler: ReturnType<typeof useAnimatedScrollHandler>
+  headerText: string
+}
+
+const AnimatedFlatList: React.FC<AnimatedFlatListProps> = ({
+  data,
+  loading,
+  isRefreshing,
+  onRefresh,
+  onSearch,
+  translateY,
+  scrollHandler,
+  onItemPress,
+  headerText,
+}) => {
+  const navigation = useNavigation()
+  const dispatch = useDispatch()
+
+  return (
+    <View style={{ flex: 1, gap: spacing.md }}>
+      {loading ? (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+          <ActivityIndicator size="large" color={colors.tint} />
+        </View>
+      ) : (
+        <View style={$contentContainer}>
+          <Text
+            style={{
+              backgroundColor: colors.background,
+              borderWidth: 1,
+              borderRadius: 8,
+              marginHorizontal: spacing.md,
+              textAlign: "center",
+            }}
+            preset="formLabel"
+            text={data.length + ` ${headerText}`}
+          />
+
+          <Animated.FlatList
+            contentContainerStyle={{
+              gap: 12,
+              paddingVertical: spacing.md,
+              marginHorizontal: spacing.md,
+            }}
+            onScroll={scrollHandler}
+            scrollEventThrottle={16}
+            keyboardDismissMode="on-drag"
+            refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />}
+            showsVerticalScrollIndicator={false}
+            data={data}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                activeOpacity={1}
+                delayPressIn={100}
+                onPress={() => onItemPress(item)}
+              >
+                <Card
+                  ContentComponent={
+                    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                      <View style={{ gap: 16 }}>
+                        <Text preset="default" text={item.title} />
+
+                        {item.image && (
+                          <View
+                            style={{
+                              flexDirection: "row",
+                              alignItems: "center",
+                              justifyContent: "space-evenly",
+                            }}
+                          >
+                            <Image
+                              source={{ uri: item.image }}
+                              resizeMode="contain"
+                              style={{ width: 100, height: 100, borderRadius: 16 }}
+                            />
+                          </View>
+                        )}
+
+                        {item.price && (
+                          <Text size="lg" preset="subheading" text={"$" + item.price} />
+                        )}
+
+                        {item.qrcode && (
+                          <Text preset="formHelper" text={"Product Code: " + item.qrcode} />
+                        )}
+                      </View>
+                    </View>
+                  }
+                />
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+      )}
+    </View>
+  )
+}
+
+export default AnimatedFlatList
+
+const $contentContainer: ViewStyle = {
+  flex: 1,
+  gap: spacing.md,
+}
--- app/screens/LNL/ProductScreen.tsx
+++ app/screens/LNL/ProductScreen.tsx
@@ -1,23 +1,13 @@
 /* eslint-disable react/jsx-key */
-import React, { useRef, useState } from "react"
-import { Card } from "../../components"
-import {
-  View,
-  FlatList,
-  Image,
-  TouchableOpacity,
-  ViewStyle,
-  RefreshControl,
-  ActivityIndicator,
-  Platform, Dimensions, ImageStyle,
-} from "react-native"
+import React, { useState } from "react"
+import { View, TouchableOpacity, ViewStyle, ActivityIndicator } from "react-native"
 import SearchBar from "../../components/SearchBar"
 import { useEffect } from "react"
 import { colors, spacing } from "../../theme"
-import { NavigationContainer, useNavigation } from "@react-navigation/native"
+import { useNavigation } from "@react-navigation/native" // Eksik import ekledik
 import { useDispatch, useSelector } from "react-redux"
 import { addProduct, changeSearchTerm, resetProducts } from "../../store/index"
-import { ListItem, Screen, Text } from "../../components"
+import { Screen } from "../../components"
 import { createSelector } from "@reduxjs/toolkit"
 import { AntDesign } from "@expo/vector-icons"
 import Animated, {
@@ -27,17 +17,10 @@ import Animated, {
   useSharedValue,
   withTiming,
 } from "react-native-reanimated"
-import Toast from "react-native-toast-message"
-import { DrawerLayout, DrawerState } from "react-native-gesture-handler"
-import { DrawerIconButton } from "../DemoShowroomScreen/DrawerIconButton"
-import { isRTL } from "../../i18n"
-import { useSafeAreaInsetsStyle } from "../../utils/useSafeAreaInsetsStyle"
-import drawerNavigator from "../../navigators/DrawerNavigator"
-import DrawerNavigator from "../../navigators/DrawerNavigator"
-import * as Demos from "../DemoShowroomScreen/demos"
+
+import AnimatedFlatList from "../../components/AnimatedFlatList" // Eksik import ekledik
 
 const ProductScreen = () => {
-
   const navigation = useNavigation()
   const dispatch = useDispatch()
 
@@ -81,18 +64,12 @@ const ProductScreen = () => {
 
   const scrollHandler = useAnimatedScrollHandler({
     onScroll: (event) => {
-      if (
-        lastContentOffset.value > event.contentOffset.y &&
-        isScrolling.value
-      ) {
+      if (lastContentOffset.value > event.contentOffset.y && isScrolling.value) {
         translateY.value = 0
-        console.log("scrolling up")
-      } else if (
-        lastContentOffset.value < event.contentOffset.y &&
-        isScrolling.value
-      ) {
+        //console.log("scrolling up")
+      } else if (lastContentOffset.value < event.contentOffset.y && isScrolling.value) {
         translateY.value = 100
-        console.log("scrolling down")
+        // console.log("scrolling down")
       }
       lastContentOffset.value = event.contentOffset.y
     },
@@ -121,7 +98,6 @@ const ProductScreen = () => {
       setLoading(true)
       dispatch(resetProducts())
 
-
       const response = await fetch("https://fakestoreapi.com/products")
       const json = await response.json()
 
@@ -136,7 +112,7 @@ const ProductScreen = () => {
       dispatch(addProduct(simplifiedProducts))
       setLoading(false)
 
-//      setSearchResults(simplifiedProducts)
+      //      setSearchResults(simplifiedProducts)
     } catch (error) {
       console.error(error)
     }
@@ -150,142 +126,70 @@ const ProductScreen = () => {
 
   const handleSearch = (searchTerm) => {
     dispatch(changeSearchTerm(searchTerm))
+  }
 
+  const onItemPress = (item) => {
+    navigation.navigate("ProductDetail", { item })
   }
 
   return (
-
     <Screen preset="fixed" safeAreaEdges={["top"]} contentContainerStyle={$screenContainer}>
+      <View style={{ flex: 1, gap: spacing.md }}>
+        <SearchBar onSearch={handleSearch} searchTerm={searchTerm} />
 
-    <View style={{  flex: 1,  gap: spacing.md  }}>
-
-      <SearchBar onSearch={handleSearch} searchTerm={searchTerm} />
-
-      {loading ? (
-
-        <View style={{flex:1, alignItems: "center", justifyContent: "center"}}>
-          <ActivityIndicator size="large"  color={colors.tint} />
-        </View>
-
-      ) : (
-        <View style={$contentContainer}>
-
-          <Text style={{backgroundColor:colors.background,borderWidth:1,borderRadius:8,marginHorizontal:spacing.md, textAlign:"center"}} preset="header" text={filteredProducts.length + " products are listed."} />
-
-          <Animated.FlatList
-
-            contentContainerStyle={{ gap: 12, paddingVertical: spacing.md, marginHorizontal: spacing.md, }} onScroll={scrollHandler} scrollEventThrottle={16}
-            keyboardDismissMode="on-drag"
-            refreshControl={
-              <RefreshControl
-                refreshing={isRefreshing}
-                onRefresh={() => {
-                  setIsRefreshing(true)
-                  getProducts()
-                  setIsRefreshing(false)
-                }}
-
-              />
-            }
-            showsVerticalScrollIndicator={false} data={filteredProducts}
-            keyExtractor={(item) => item.id.toString()} renderItem={({ item }) => (
-            <TouchableOpacity
-              activeOpacity={1}
-              delayPressIn={100}
-              onPress={() => {
-                // Ürün detay sayfasına yönlendirme işlemi
-                navigation.navigate("ProductDetail", { item })
+        {loading ? (
+          <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <ActivityIndicator size="large" color={colors.tint} />
+          </View>
+        ) : (
+          <View style={$contentContainer}>
+            <AnimatedFlatList // Animasyonlu FlatList kullanımı
+              data={filteredProducts}
+              loading={loading}
+              isRefreshing={isRefreshing}
+              onRefresh={() => {
+                setIsRefreshing(true)
+                getProducts()
+                setIsRefreshing(false)
               }}
-            >
-              <Card ContentComponent={
+              onSearch={handleSearch}
+              translateY={translateY}
+              scrollHandler={scrollHandler}
+              onItemPress={onItemPress} // onItemPress işlevini ilettik
+              headerText="Products are Listed"
+            />
 
-                <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-
-                  <View style={{ gap: 16 }}>
-
-                    <Text preset="default" text={item.title} />
-
-                    <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-evenly" }}>
-                      <Image
-                        source={{ uri: item.image }}
-                        resizeMode="contain" // ya da "contain" veya "stretch"
-                        style={{ width: 100, height: 100, borderRadius: 16 }}
-                      />
-                      <Text size="lg" preset="subheading" text={"$" + item.price} />
-                    </View>
-
-                    <Text preset="formHelper" text={"Product Code: " + item.qrcode} />
-
-                  </View>
-
-                </View>
-              }
-              />
-            </TouchableOpacity>
-          )}
-          />
-
-
-          <Animated.View style={[$action, addButtonStyle]}>
-            <TouchableOpacity onPress={() => {
-              navigation.navigate("ProductAdd") }}
-            >
-              <AntDesign name="pluscircle" size={48} color={colors.tint} />
-            </TouchableOpacity>
-          </Animated.View>
-        </View>
-      )}
-
-    </View>
+            <Animated.View style={[$action, addButtonStyle]}>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("ProductAdd")
+                }}
+              >
+                <AntDesign name="pluscircle" size={48} color={colors.tint} />
+              </TouchableOpacity>
+            </Animated.View>
+          </View>
+        )}
+      </View>
     </Screen>
-
-
   )
-
-
 }
-
 
 export default ProductScreen
 
 const $action: ViewStyle = {
-
   alignItems: "center",
   position: "absolute",
   left: 0,
   right: 0,
   bottom: 16,
-
-
 }
 
 const $contentContainer: ViewStyle = {
-  flex:1,
-  gap: spacing.md
+  flex: 1,
+  gap: spacing.md,
 }
 
 const $screenContainer: ViewStyle = {
   flex: 1,
-
 }
-const $drawer: ViewStyle = {
-  backgroundColor: colors.background,
-  flex: 1,
-}
-const $logoImage: ImageStyle = {
-  height: 150,
-  width: 150,
-}
-const $flatListContentContainer: ViewStyle = {
-  paddingHorizontal: spacing.lg,
-}
-
-const $logoContainer: ViewStyle = {
-  alignSelf: "flex-start",
-  justifyContent: "center",
-  height: 56,
-  paddingHorizontal: spacing.lg,
-}
-
-
-// @demo remove-file
